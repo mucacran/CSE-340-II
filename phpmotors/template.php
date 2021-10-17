0cbd8404ba2ec6f7e585d4c2553025356bdf7f66
mucacran <el.mucacran.rasta@hotmail.com>
2021-10-17 13:45:41 -0500

    no es necesario

--- phpmotors/template.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $ruta = $_SERVER['DOCUMENT_ROOT'] . '/CSE-340-II/phpmotors/snippets/';
-    $pages = $_SERVER['DOCUMENT_ROOT'] . '/CSE-340-II/phpmotors/';
-?>
-    <?php require $ruta . 'header.php'; ?>
-
-    <h1>Here content</h1>
-
-    <?php require $ruta . 'footer.php'; ?>
